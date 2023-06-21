9e604884935e50f6dd0979344c541dd836e66398
Jujubis94 <113546537+Jujubis94@users.noreply.github.com>
2023-06-21 15:55:23 +0200

    Done

--- Shield.java
+++ Shield.java
@@ -17,15 +17,19 @@ public class Shield {
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
 
-        List<Hero> elders;
+        List<Hero> elders = heroes.stream()
+        .filter(h -> h.getAge() > 59)
+        .collect(Collectors.toList());
         // TODO 1 : filter heroes in order to found heroes older than 59
 
         System.out.println("\nElders:");
         for (Hero elder : elders) {
             System.out.println(elder.getName());
         }
-
-        List<Hero> intolerants;
+            
+            List<Hero> intolerants = heroes.stream()
+            .filter(h -> h.isGlutenIntolerant() == true)
+            .collect(Collectors.toList());
         // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
 
         System.out.println("\nGluten intolerants:");
